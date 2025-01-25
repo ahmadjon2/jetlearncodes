100fce4255bed1ed382f5736f69b4df43209bd3b
ahmadjon2 <khamro.ruziev@gmail.com>
2025-01-25 18:06:10 +0000

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,15 @@
+def fibonachi_s(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibonachi_s(n-1)+fibonachi_s(n-2)
+print (fibonachi_s(8))
+
+def factorial(f):
+    if f == 1 :
+        return 1
+    else:
+        return f*factorial(f-1)
+print (factorial(10
+                 
+                 ))
